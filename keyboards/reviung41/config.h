6d992c62c269988251ce3c4e39203de5b7278184
Sean Santry <sean@santry.us>
2022-01-17 23:41:17 -0500

    Update reviung41 for wiring of reviung41lp (rows swapped)

--- keyboards/reviung41/config.h
+++ keyboards/reviung41/config.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 #define MATRIX_ROW_PINS \
-    { F4, F5, F6, F7, B1, B3, B2 }
+    { F4, F5, F6, B2, B3, B1, F7 }
 #define MATRIX_COL_PINS \
     { D4, C6, D7, E6, B4, B5 }
 #define UNUSED_PINS
